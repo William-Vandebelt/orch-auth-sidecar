ffe36bec5de9e93f20ed639b7d0b961fd5d399ae
William Vandebelt <wvandebelt@salesforce.com>
2022-03-17 23:00:51 -0400

    :sparkles: parse and print custom claims

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/quasilyte/go-ruleguard/dsl"
 	kit_utils "github.com/sailsforce/gomicro-kit/utils"
+	jose "gopkg.in/square/go-jose.v2/jwt"
 )
 
 func init() {
@@ -13,6 +17,45 @@ func init() {
 	}
 }
 
+type JwtClaims struct {
+	IssuerId          string            `json:"iss"`
+	IssuerType        int               `json:"ist"`
+	AudienceId        string            `json:"aud"`
+	AudienceType      string            `json:"aut"`
+	JwtExpiration     *jose.NumericDate `json:"exp"`
+	JwtValidNotBefore *jose.NumericDate `json:"nbf"`
+	JwtIssuedAt       *jose.NumericDate `json:"iat"`
+	JwtId             string            `json:"jti"`
+	CallerContext     string            `json:"ctx"`
+	SubjectType       string            `json:"sty"`
+	Subject           string            `json:"sub"`
+	OrchClaims
+}
+
+type OrchClaims struct {
+	AccountId string `json:"org62.account_id"`
+	ContactId string `json:"org62.contact_id"`
+}
+
 func main() {
-	log.Println("hello world")
+	// pass token using the '-token={jwt token} flag'
+	tokenPtr := flag.String("token", "", "jwt token")
+	flag.Parse()
+
+	// parse token
+	jwtToken, err := jose.ParseSigned(*tokenPtr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// pull out claims
+	claims := JwtClaims{}
+	if err := jwtToken.UnsafeClaimsWithoutVerification(&claims); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	log.Printf("claims: %+v\n", claims)
+
+	os.Exit(0)
 }
